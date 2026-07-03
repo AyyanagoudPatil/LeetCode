2f20f62186f265a475addd9c8f9b289692058dbe
Ayyanagoud Patil <ayyanagoudpatil311@gmail.com>
2026-07-03 21:31:04 +0530

    Time: 8 ms (65.30%) | Memory: 12.3 MB (56.85%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        integer={
+            'I': 1,
+            'IV': 4,
+            'V': 5,
+            'IX': 9,
+            'X': 10,
+            'XL': 40,
+            'L': 50,
+            'XC': 90,
+            'C': 100,
+            'CD': 400,
+            'D': 500,
+            'CM': 900,
+            'M': 1000
+        }
+        result = 0
+        for i in range(len(s)):
+            if i < len(s) - 1 and integer[s[i]] < integer[s[i + 1]]:
+                result -= integer[s[i]]
+            else:
+                result += integer[s[i]]
+        return result
