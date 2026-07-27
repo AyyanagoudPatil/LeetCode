5341e7d99ba737a56808f21c5a07cde7e092704e
Ayyanagoud Patil <ayyanagoudpatil311@gmail.com>
2026-07-28 01:15:52 +0530

    Time: 1341 ms (44.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,12 @@
+/* Write your PL/SQL query statement below */
+select s.name 
+from SalesPerson s 
+where s.sales_id not in (
+    select o.sales_id 
+    from Orders o 
+    where o.com_id = (
+        select c.com_id 
+        from Company c 
+        where c.name = 'RED'
+    )
+)
